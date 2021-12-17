6bc82e96902814a1077792aa76ab849446e46b62
Andrew Bell <abell5@g.clemson.edu>
2021-12-17 14:39:32 -0500

    Refactor looping

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from time import sleep
 from bs4 import BeautifulSoup as bs
 import random
 import yaml
+import sys
 
-with open(r'creds.yaml') as file:
-    creds = yaml.load(file, Loader=yaml.FullLoader)
+#with open(r'creds.yaml') as file:
+#    creds = yaml.load(file, Loader=yaml.FullLoader)
 
 driver = webdriver.Chrome(executable_path='/Users/andrew/Documents/workspace/instabot/chromedriver/chromedriver')
 
 driver.get('https://www.instagram.com')
 
-username = creds['username']
-password = creds['password']
+username = sys.argv[1]
+password = sys.argv[2]
+
+print(f":::{username}:::")
 
 min_wait_time = 6
 max_wait_time = 8
@@ -38,65 +41,69 @@ login_button.click()
 
 sleep(get_wait_time(min_wait_time,max_wait_time))
 
-profiles = ['stefan_froehlich','thefilmmagazine','nibera_35mm','neilkrug','evazubeck','23h46min','retronyc','f1rstoftheroll','analogue_people','filmphotomag','thomhavlik','magazine35mm','in.film.we.trust','prianalog','film.wave','shootfilmmag']
+profiles = ['nowness','zhaomingwuartist','vgiarrano','zinlimart','seancheetham','drawingcenter','milohartnoll','art','artbasel','laafa','igorlipskykh','contemporary.paintings','jvan_rviz','contemporaryfigurativeart','piddy1972','stephenbaumanartwork','polina_abdulaeva_art','philhalestudio','jlou.is','alan_mcgowan_art','stefan_froehlich','thefilmmagazine','nibera_35mm','neilkrug','evazubeck','23h46min','retronyc','f1rstoftheroll','analogue_people','filmphotomag','thomhavlik','magazine35mm','in.film.we.trust','prianalog','film.wave','shootfilmmag']
 random.shuffle(profiles)
 
 likes = 1
 
-while likes <= 30:
-    for p in profiles:
-        driver.get(f'https://www.instagram.com/{p}/')
-        
-        sleep(get_wait_time(min_wait_time,max_wait_time))
-        
-        followers = driver.find_element_by_xpath("//a[@href='/{}/followers/']".format(p))
-        followers.click()
-        
-        sleep(get_wait_time(min_wait_time,max_wait_time))
-        
-        fBody  = driver.find_element_by_xpath("//div[@class='isgrP']")
-        
-        scroll = 0
-        while scroll < 10:
-            driver.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', fBody)
-            sleep(1)
-            scroll += 1
+for p in profiles:
+    print(f"::searching {p}::")
+    driver.get(f'https://www.instagram.com/{p}/')
+    
+    sleep(get_wait_time(min_wait_time,max_wait_time))
+    
+    followers = driver.find_element_by_xpath("//a[@href='/{}/followers/']".format(p))
+    followers.click()
+    
+    sleep(get_wait_time(min_wait_time,max_wait_time))
+    
+    fBody  = driver.find_element_by_xpath("//div[@class='isgrP']")
+    
+    scroll = 0
+    while scroll < 10:
+        driver.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', fBody)
+        sleep(1)
+        scroll += 1
 
-        fList = [my_elem.get_attribute("href") for my_elem in driver.find_elements_by_xpath("//div[@class='isgrP']//a")]
-        fList_unique = list(dict.fromkeys(fList))
-        random.shuffle(fList_unique)
-        
-        for f in fList_unique:
-            driver.get(f)
-                       
-            sleep(get_wait_time(min_wait_time,max_wait_time))
-            
-            driver.execute_script("window.scrollTo(0, 200);")
-            
-            sleep(get_wait_time(min_wait_time,max_wait_time))
-            
-            try:
-                pic = driver.find_element_by_class_name("kIKUG")  
-                pic.click()
-            
-                sleep(get_wait_time(min_wait_time,max_wait_time))
-                
-                like = driver.find_element_by_class_name('fr66n')
-                soup = bs(like.get_attribute('innerHTML'),'html.parser')
-                if(soup.find('svg')['aria-label'] == 'Like'):
-                    like.click()
-                    likes+=1
-                    print("Like counter: ", likes)
+    fList = [my_elem.get_attribute("href") for my_elem in driver.find_elements_by_xpath("//div[@class='isgrP']//a")]
+    fList_unique = list(dict.fromkeys(fList))
+    random.shuffle(fList_unique)
+    
+    for f in fList_unique:
+        driver.get(f)
                     
-                sleep(get_wait_time(min_wait_time,max_wait_time))               
-                
-            except:
-                print("No pics")
-                
-            if likes >= 30:
-                break
+        sleep(get_wait_time(min_wait_time,max_wait_time))
         
-            sleep(30)
+        driver.execute_script("window.scrollTo(0, 200);")
+        
+        sleep(get_wait_time(min_wait_time,max_wait_time))
+        
+        try:
+            pic = driver.find_element_by_class_name("kIKUG")  
+            pic.click()
+        
+            sleep(get_wait_time(min_wait_time,max_wait_time))
+            
+            like = driver.find_element_by_class_name('fr66n')
+            soup = bs(like.get_attribute('innerHTML'),'html.parser')
+            if(soup.find('svg')['aria-label'] == 'Like'):
+                like.click()
+                likes+=1
+                print("Like counter: ", likes)
+                
+            sleep(get_wait_time(min_wait_time,max_wait_time))               
+            
+        except:
+            print("No pics")
+            
+        if likes >= 30:
+            break
+
+        sleep(30)
+    
+    if likes >= 30:
+        break
+        
         
 
 driver.close()
